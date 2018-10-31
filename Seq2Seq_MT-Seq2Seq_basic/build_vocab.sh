0f61e847f620b00cf66773c92eba236094650bcc
Ubuntu <ubuntu@ip-172-31-9-25.us-east-2.compute.internal>
2018-10-31 18:58:40 +0000

    repo structure reorg --vikas

--- Seq2Seq_MT-Seq2Seq_basic/build_vocab.sh
+++
@@ -1,1 +0,0 @@
-python3 vocab.py --train-src ../data/train.en-az.en.txt  --train-tgt ../data/train.en-az.az.txt --vocab-type 'bpe' --size 10000 data/vocab.bin 
---
+++ scripts/build_vocab.sh
@@ -0,0 +1,1 @@
+python3 ../models/vocab.py --train-src ../data/train.en-az.az.txt  --train-tgt ../data/train.en-az.en.txt --vocab-type 'word' --size 10000 ../data/vocab_az-en.bin 
