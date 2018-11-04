aaa738afa460056afb949ae1d766d5e6de86368b
Shikib Mehri <mehrishikib@gmail.com>
2018-11-04 18:38:06 -0500

    backtranslation prep

--- scripts/build_vocab.sh
+++ scripts/build_vocab.sh
@@ -1,4 +1,8 @@
 lang_pair=${@:1}
 source=${lang_pair##*-}
+target=${lang_pair%-*}
 
-python3 ../models/vocab.py --train-src ../data/train.$lang_pair.$source.txt  --train-tgt ../data/train.$lang_pair.en.txt --vocab-type 'word' --size 50000 ../data/vocab_$lang_pair.bin 
+echo $source
+echo $target
+
+python3 ../models/vocab.py --train-src ../data/train.$lang_pair.$source.txt  --train-tgt ../data/train.$lang_pair.$target.txt --vocab-type 'word' --size 50000 ../data/vocab_$lang_pair.bin 
--- wikipedia.sh
+++ wikipedia.sh
@@ -6,14 +6,15 @@
 LANGUAGE=$1
 
 #download wikipedia dump
-wget https://dumps.wikimedia.org/${LANGUAGE}wiki/20181001/${LANGUAGE}wiki-20181001-pages-articles-multistream.xml.bz2 .
+#wget https://dumps.wikimedia.org/${LANGUAGE}wiki/20181001/${LANGUAGE}wiki-20181001-pages-articles-multistream.xml.bz2 .
+wget https://dumps.wikimedia.org/enwikinews/20181101/enwikinews-20181101-pages-articles-multistream.xml.bz2 .
 
 #extract dump
-bzip2 -d ${LANGUAGE}wiki-20181001-pages-articles-multistream.xml.bz2
+bzip2 -d ${LANGUAGE}wikinews-20181101-pages-articles-multistream.xml.bz2
 
 #extract articles in one big file
 cd $WIKI_EX
-python WikiExtractor.py -b 100000000000 ../${LANGUAGE}wiki-20181001-pages-articles-multistream.xml -o ..
+python WikiExtractor.py -b 100000000000 ../${LANGUAGE}wikinews-20181101-pages-articles-multistream.xml -o ..
 cd ..
 mv AA/wiki_00 ${LANGUAGE}.wiki.txt
 rm -Rf AA
