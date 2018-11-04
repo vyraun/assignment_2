e15bbf32597c653dc471d456c2aa7592ae8a2412
Vikas Raunak <vyraun@gmail.com>
2018-11-04 15:49:17 -0500

    adding training for all langs

--- train_fasttext.sh
+++ train_fasttext.sh
@@ -1,1 +1,3 @@
-./fasttext skipgram -input ../wikis/az.wiki.txt -output fasttext_vectors -epoch 10 -dim 256 -minCount 1
+./fasttext skipgram -input ../wikis/az.wiki.txt -output az_fasttext_vectors -epoch 10 -dim 256 -minCount 1
+./fasttext skipgram -input ../wikis/be.wiki.txt -output be_fasttext_vectors -epoch 10 -dim 256 -minCount 1
+./fasttext skipgram -input ../wikis/gl.wiki.txt -output gl_fasttext_vectors -epoch 10 -dim 256 -minCount 1
