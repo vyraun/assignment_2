85d562f549b191b36067971c3391a9f9c9be7689
Vikas Raunak <vyraun@gmail.com>
2018-11-04 15:17:59 -0500

    Update train_fasttext.sh

--- train_fasttext.sh
+++ train_fasttext.sh
@@ -1,1 +1,1 @@
-./fasttext skipgram -input ../az.wiki.txt -output fasttext_vectors -epoch 10 -dim 300 -minCount 1
+./fasttext skipgram -input wikis/az.wiki.txt -output fasttext_vectors -epoch 10 -dim 256 -minCount 1
